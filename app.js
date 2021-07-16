fd324ad8041792cbb666652285b5d13ccabb7fb0
tommywu10 <tomwu10@gmail.com>
2021-07-15 23:20:56 -0400

    idk

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-var input = document.querySelector('input');
 
 var bar = new ProgressBar.Circle(container, {
-    zIndex: 2,
     color: '#aaa',
-    // This has to be the same size as the maximum width to
-    // prevent clipping
     strokeWidth: 4,
     trailWidth: 1,
     easing: 'easeInOut',
@@ -14,25 +10,26 @@ var bar = new ProgressBar.Circle(container, {
     },
     from: { color: '#aaa', width: 1 },
     to: { color: '#333', width: 4 },
-    // Set default step function for all animate calls
     step: function(state, circle) {
       circle.path.setAttribute('stroke', state.color);
       circle.path.setAttribute('stroke-width', state.width);
   
-      var value = Math.round(circle.value() * 100);
+      var value = Math.round(circle.value() * 100); 
+
       if (value === 0) {
         circle.setText('');
       } else {
         circle.setText(value);
       }
+
   
     }
   });
   bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
   bar.text.style.fontSize = '2rem';
-  bar.text.style.zIndex = '1';
   
-  bar.animate(1.0);  // Number from 0.0 to 1.0
+  bar.animate(.75);  //<<----change value here ************************
+  
 
   console.log(bar._progressPath._opts.duration)
 
@@ -70,7 +67,7 @@ var bar = new ProgressBar.Circle(container, {
         shapes: "square",
         spread: 360,
         startVelocity: 25,
-        ticks: 100
+        ticks: 100,
         });
         
     fire(1, {
@@ -82,5 +79,5 @@ var bar = new ProgressBar.Circle(container, {
         shapes: "square",
         spread: 360,
         startVelocity: 30,
-        ticks: 100
+        ticks: 100,
     });
